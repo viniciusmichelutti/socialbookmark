ce55192bcfd82d3e69fec4b83abe2d451b3b4579
Vinicius Michelutti <viniciusmichelutti@gmail.com>
2017-04-19 00:53:02 -0300

    Implemented the forgotten password views and urls

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from django.contrib.auth.views import logout_then_login, LoginView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import logout_then_login, LoginView, PasswordChangeView, PasswordChangeDoneView, \
+    PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 from account import views
 
@@ -9,4 +10,8 @@ urlpatterns = [
     url(r'^logout/$', logout_then_login, name='logout'),
     url(r'^change-password/$', PasswordChangeView.as_view(), name='change_password'),
     url(r'^change-password/done$', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    url(r'^password-reset/$', PasswordResetView.as_view(), name='password_reset'),
+    url(r'^password-reset/done$', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)$', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^password-reset/complete$', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
