800d47dcb790a5da94b1fce9e1d61174965dc028
Vinicius Michelutti <viniciusmichelutti@gmail.com>
2017-04-19 00:52:38 -0300

    Simple django backend to print out sended emails

--- socialbookmark/settings.py
+++ socialbookmark/settings.py
@@ -32,6 +32,8 @@ LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_URL = reverse_lazy('logout')
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Application definition
 
 INSTALLED_APPS = [
