4da013eaaff54878115d429b01665a6a829bf094
Vinicius Michelutti <viniciusmichelutti@gmail.com>
2017-04-18 23:48:06 -0300

    Redirect logged users to the home

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout_then_login
+from django.contrib.auth.views import logout_then_login, LoginView
 
 from account import views
 
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
-    url(r'^login/$', login, name='login'),
+    url(r'^login/$', LoginView.as_view(redirect_authenticated_user=True) , name='login'),
     url(r'^logout/$', logout_then_login, name='logout'),
 ]
