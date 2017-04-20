d686b88f6dfcf6cbb74804d7d510e1543f6e87c4
Vinicius Michelutti <viniciusmichelutti@gmail.com>
2017-04-20 01:15:27 -0300

    Saving facebook photo to our profile

--- account/auth_pipeline.py
+++ account/auth_pipeline.py
@@ -1,6 +1,22 @@
+from django.core.files.base import ContentFile
+from requests import request
+from requests.exceptions import HTTPError
+
 from account.models import Profile
 
 
 def save_profile(backend, user, response, *args, **kwargs):
     if backend.name == 'facebook' and kwargs['is_new']:
-        Profile.objects.create(user=user)
+        profile = Profile.objects.create(user=user)
+
+        url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
+        try:
+            response = request('GET', url, params={'type': 'large'})
+            response.raise_for_status()
+        except HTTPError:
+            pass
+        else:
+            profile.photo.save('{0}_social.jpg'.format(user.username),
+                                       ContentFile(response.content))
+            profile.save()
+            
