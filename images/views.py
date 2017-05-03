5d2318e7e7bbdb8c942b4b780d72fc4fbfe8bd6f
Vinícius Michelutti <vinicius@meuspedidos.com.br>
2017-05-03 10:32:18 -0300

    Some stylish

--- images/views.py
+++ images/views.py
@@ -52,7 +52,7 @@ def image_like(request):
 @login_required
 def image_list(request):
     images = Image.objects.all()
-    paginator = Paginator(images, 5)
+    paginator = Paginator(images, 15)
     page = request.GET.get('page')
     try:
         images = paginator.page(page)
