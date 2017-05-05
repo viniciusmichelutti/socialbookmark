45f5d22b24794868c9dceac292858c1558137268
Vinícius Michelutti <vinicius@meuspedidos.com.br>
2017-05-05 15:52:15 -0300

    Targets for action

--- actions/models.py
+++ actions/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -6,6 +8,9 @@ class Action(models.Model):
     user = models.ForeignKey(User, related_name='actions', db_index=True)
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
+    target_ct = models.ForeignKey(ContentType, null=True, blank=True, related_name="target_obj")
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey('target_ct', 'target_id')
 
     class Meta:
         ordering = ('-created',)
